b0a2e61a03e6fe1baffeb7857573eb2dc260ba19
patrick <plyons72@gmail.com>
2018-06-02 14:44:27 -0400

    take in username as well

--- src/main/java/TexasHoldem.java
+++ src/main/java/TexasHoldem.java
@@ -5,33 +5,46 @@ Alex McMullen
 Gary Xu
  */
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 public class TexasHoldem {
 
     public static void main(String[] args)
     {
         TexasHoldem texasHoldem = new TexasHoldem();
-        int numCPUs;
+        int numCPUs = 0;
+
         boolean validNum = true;
+
         String[] cpuNames;
 
+        String username;
+
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Welcome to Texas Holdem! How many oponents would you like to face (between 1 and 7 only): ");
 
+        //TODO: Check for non integer input
+        //and allow user to continue entering values until they get it right
         do{
             validNum = true;
             numCPUs = scan.nextInt();
+            scan.nextLine();
+
             if(numCPUs > 7 || numCPUs < 1) {
                 validNum = false;
                 System.out.print("Error! Number of opponents must be between 1 and 7. Please enter a valid number: ");
             }
 
         }while(!validNum);
+
         cpuNames = texasHoldem.getNames(numCPUs);
 
+        System.out.print("\nWhat is your name?: ");
+        username = scan.nextLine();
 
+        System.out.printf("Your name is: %s\n", username);
+        System.out.println("Your opponents are: ");
         //For testing purposes only. Just ensures we get names accurately
         for (String word: cpuNames
              ) {System.out.printf("%s\n", word);
