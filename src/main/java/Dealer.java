8539f8b1240f0077493b94a6b6b929a748a9739e
patrick <plyons72@gmail.com>
2018-06-16 13:02:29 -0400

    Implemented better OOP Design, created dealer class to hold various aspects of the game, and created cards in instantiation of each player object, giving the dealer cards for the flop, turn, and river

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -3,49 +3,64 @@ Patrick Lyons
 Michael Hopwood
 Alex McMullen
 Gary Xu
- */
+*/
 
 public class Dealer {
 
+    // Holds the current value in the pot
     private int potValue;
 
-    public Dealer() {
-        potValue = 0;
+    // Holds the cards in the flop, turn, and river
+    private int[] ftr;
+
+    // Starting game constructor
+    public Dealer(int startingPot, int[] cards) {
+        potValue = startingPot;
+        ftr = cards;
     }
 
+    // Setters
+    public void setPotValue(int bet) { potValue += bet; }
+    public void setSharedCards(int[] cards) { ftr = cards; }
+
+    // Getters
+    public int getPotValue() { return potValue; }
+    public int[] getFTR() { return ftr; }
+
+
+    /*
+    //TODO: Implement
     // Takes in an array of player cards
     public int checkWinCon(){
 
     }
 
-    // Increment the value stored in the pot
-    public void setPotAmount(int bet) {
-        potValue += bet;
-    }
-
-    // Take in the winners current pot, add their winnings, deplete pot to 0
-    public int returnWinnings(int winnerAmount) {
-        winnerAmount += potValue;
-        potValue = 0;
-        return winnerAmount;
-    }
-
-
+    //TODO: Implement
     // If bets are equal and the players called, return an array of 3 ints
     // representing the flop
     public int[] showFlop(boolean bettingOver, int[] remainingCards) {
 
     }
 
+    //TODO: Implement
     // If bets are equal and the players called, return an int
     // representing the turn
     public int showTurn(boolean bettingOver, int[] remainingCards) {
 
     }
 
+    //TODO: Implement
     // If bets are equal and the players called, return an int
     // representing the river
     public int showRiver(boolean bettingOver, int[] remainingCards) {
 
+    }
+    */
+
+    // Take in the winners current pot, add their winnings, deplete pot to 0
+    public int returnWinnings(int winnerAmount) {
+        winnerAmount += potValue;
+        potValue = 0;
+        return winnerAmount;
     }
 }
